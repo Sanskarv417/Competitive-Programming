f77e98890bb7bf99031887ff594dfa503d5eefe3
Sanskarv417 <88963619+Sanskarv417@users.noreply.github.com>
2022-02-18 23:54:52 +0530

    Create 268.cpp

---
+++ Leetcode/268.cpp
@@ -0,0 +1,15 @@
+// 268. Missing Number
+
+class Solution {
+public:
+    int missingNumber(vector<int>& v) 
+    {
+        int n=v.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=v[i];
+        }
+        return (((n*(n+1))/2)-sum);
+    }
+};
