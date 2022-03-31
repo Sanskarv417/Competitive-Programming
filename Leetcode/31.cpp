7421f8b217b8b43fca527659b8fafe779a41170b
Sanskarv417 <88963619+Sanskarv417@users.noreply.github.com>
2022-03-31 13:30:22 +0530

    Update 31.cpp

--- Leetcode/31.cpp
+++ Leetcode/31.cpp
@@ -2,6 +2,34 @@
 
 class Solution {
 public:
+    void nextPermutation(vector<int>& v) {
+        int ind1,ind2;
+        for(int i=v.size()-2;i>=0;i--)
+        {
+            if(v[i]<v[i+1])
+            {
+                ind1=i;
+                break;
+            }
+        }
+        if(ind1<0)
+            reverse(v.begin(),v.end());
+        else
+        {
+            for(int i=v.size()-1;i>ind1;i--)
+            if(v[i]>v[ind1])
+            {
+                ind2=i;
+                break;
+            }
+        swap(v[ind1],v[ind2]);
+        reverse(v.begin()+ind1+1,v.end());
+        }
+    }
+};
+
+class Solution {
+public:
     void nextPermutation(vector<int>& nums) {
         next_permutation(nums.begin(),nums.end());
     }
